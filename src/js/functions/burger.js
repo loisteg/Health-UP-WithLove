273d24279544b458b7abb970dcad701b69227828
Loisteg <loisteg@outlook.com>
2022-09-18 04:41:17 +0300

    Added 1,2,3,4 screens without header

---
+++ src/js/functions/burger.js
@@ -0,0 +1,33 @@
+import { disableScroll } from '../functions/disable-scroll';
+import { enableScroll } from '../functions/enable-scroll';
+
+(function () {
+  const burger = document?.querySelector('[data-burger]');
+  const menu = document?.querySelector('[data-menu]');
+  const menuItems = document?.querySelectorAll('[data-menu-item]');
+
+  burger?.addEventListener('click', (e) => {
+    burger?.classList.toggle('header__burger--active');
+    menu?.classList.toggle('menu--active');
+
+    if (menu?.classList.contains('menu--active')) {
+      burger?.setAttribute('aria-expanded', 'true');
+      burger?.setAttribute('aria-label', 'Закрыть меню');
+      disableScroll();
+    } else {
+      burger?.setAttribute('aria-expanded', 'false');
+      burger?.setAttribute('aria-label', 'Открыть меню');
+      enableScroll();
+    }
+  });
+
+  menuItems?.forEach((el) => {
+    el.addEventListener('click', () => {
+      burger?.setAttribute('aria-expanded', 'false');
+      burger?.setAttribute('aria-label', 'Открыть меню');
+      burger.classList.remove('header__burger--active');
+      menu.classList.remove('menu--active');
+      enableScroll();
+    });
+  });
+})();
