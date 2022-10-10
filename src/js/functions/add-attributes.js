786ff3b6e5899c74deb4bd773a442f3cf6084e71
Loisteg <loisteg@outlook.com>
2022-10-10 16:22:47 +0300

    1.0

--- src/js/functions/add-attributes.js
+++
@@ -1,6 +0,0 @@
-const addAttributes = () => {
-  const list = document?.querySelectorAll("[data-aos");
-  list.forEach((item) => item.setAttribute("data-aos-once", "true"));
-};
-
-export default addAttributes;
--- src/js/main.js
+++
@@ -1,3 +0,0 @@
-import "./_vendor";
-import "./_functions";
-import "./_components";
